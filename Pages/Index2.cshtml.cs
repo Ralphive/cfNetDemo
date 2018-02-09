98fca8da5902fff9959a4ede6b1cac04f9ec477b
Ralph <ralphrpo@gmail.com>
2018-02-09 11:35:03 +0000

    UI Implementation

---
+++ Pages/Index2.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cfNetDemo.Pages
+{
+    public class Index2Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
